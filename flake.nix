4c71c1886ea3eccbf2dca4f78cb53c9ff21c3d30
Dušan Tichý <tichy@ics.muni.cz>
2026-01-13 20:07:41 +0100

    hm backup

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "hm-backup";
           home-manager.users.agni = import ./hosts/hippaforalkus/home.nix;
         }
       ];
