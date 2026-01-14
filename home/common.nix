8001fa71377af5887c719edc6b007b55001a4936
Dusan Tichy <tichy.dusan@proton.me>
2026-01-14 20:06:08 +0100

    plasma-manager

--- home/common.nix
+++ home/common.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  imports = [ <plasma-manager/modules> ];
+
   programs.fish = {
     enable = true;
     shellAliases = {
@@ -21,6 +23,29 @@
       user.name = "Dušan Tichý";
       init.defaultBranch = "main";
       push.autoSetupRemote = true;
+    };
+  };
+
+  programs.plasma = {
+    enable = true;
+    shortcuts = {
+      ActivityManager.switch-to-activity-8a2192d1-b4bb-412c-8906-37fc2733703b = [ ];
+      "KDE Keyboard Layout Switcher"."Switch to Next Keyboard Layout" = "Meta+Space";
+    };
+    configFile = {
+      kcminputrc."Libinput/5426/195/Razer Razer DeathAdder V3 Pro".PointerAccelerationProfile = 1;
+      kcminputrc."Libinput/5426/195/Razer Razer DeathAdder V3 Pro Mouse".PointerAccelerationProfile = 1;
+      kdeglobals.General.BrowserApplication = "brave-browser.desktop";
+      kdeglobals.General.TerminalApplication = "/nix/store/4fkwvnja07zmivbi0l68351rzd3166hp-ghostty-1.2.3/bin/ghostty --gtk-single-instance=true";
+      kdeglobals.General.TerminalService = "com.mitchellh.ghostty.desktop";
+      ksmserverrc.General.loginMode = "emptySession";
+      kwalletrc.Wallet."First Use" = false;
+      plasmanotifyrc."Applications/brave-browser".Seen = true;
+      plasmanotifyrc.Notifications.LowPriorityHistory = true;
+    };
+    workspace = {
+      lookAndFeel = "org.kde.breezedark.desktop";
+      wallpaper = "${pkgs.kdePackages.plasma-workspace-wallpapers}/share/wallpapers/Honeywave/contents/images/5120x2880.png";
     };
   };
 
