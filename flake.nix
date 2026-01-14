806a1b2aefcb4a16ac1c0f2ca36cededf44efbc5
Dusan Tichy <tichy.dusan@proton.me>
2026-01-14 20:09:22 +0100

    plasma manager to flake 3

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "hm-backup";
-            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+            home-manager.sharedModules = [ plasma-manager.homeModules.plasma-manager ];
             home-manager.users.agni = import ./hosts/hippaforalkus/home.nix;
           }
         ];
@@ -50,7 +50,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "hm-backup";
-            home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+            home-manager.sharedModules = [ plasma-manager.homeModules.plasma-manager ];
             home-manager.users.agni = import ./hosts/destiny/home.nix;
           }
         ];
