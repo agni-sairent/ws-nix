438f81b26f782b36b20e5bf90e0fa5dc50901a68
Dusan Tichy <tichy.dusan@proton.me>
2026-01-14 20:08:37 +0100

    plasma manager to flake 2

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
   outputs =
@@ -14,6 +19,7 @@
       self,
       nixpkgs,
       home-manager,
+      plasma-manager,
       ...
     }@inputs:
     {
