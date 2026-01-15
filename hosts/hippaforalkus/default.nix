9c867068e9739fec7e8e6eb4aa3eaa67b1c50cd6
Dušan Tichý <tichy@ics.muni.cz>
2026-01-15 09:56:08 +0100

    add jdk25 and maven to hippaforalkus

--- hosts/hippaforalkus/default.nix
+++ hosts/hippaforalkus/default.nix
@@ -10,7 +10,10 @@
   # Host specific packages
   programs.firefox.enable = true;
   environment.systemPackages = with pkgs; [
+    # Java
     jdk21_headless
+    jdk25_headless
+    maven
   ];
 
   # DO NOT CHANGE: Tracks the NixOS version this host was first installed with.
