8be480f28326e6ff880f6ea2c087be3185e479d1
Dusan Tichy <tichy.dusan@proton.me>
2026-01-17 11:05:52 +0100

    czech locale correct format

--- common/default.nix
+++ common/default.nix
@@ -5,20 +5,20 @@
   time.timeZone = "Europe/Prague";
 
   i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocales = [ "cs_CZ.UTF-8/UTF-8" ];
+  i18n.extraLocales = [ "cs_CZ.UTF-8" ];
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "cs_CZ.UTF-8/UTF-8";
-    LC_IDENTIFICATION = "cs_CZ.UTF-8/UTF-8";
-    LC_MEASUREMENT = "cs_CZ.UTF-8/UTF-8";
-    LC_MONETARY = "cs_CZ.UTF-8/UTF-8";
-    LC_NAME = "cs_CZ.UTF-8/UTF-8";
-    LC_NUMERIC = "cs_CZ.UTF-8/UTF-8";
-    LC_PAPER = "cs_CZ.UTF-8/UTF-8";
-    LC_TELEPHONE = "cs_CZ.UTF-8/UTF-8";
-    LC_TIME = "cs_CZ.UTF-8/UTF-8";
-    LC_MESSAGES = "cs_CZ.UTF-8/UTF-8";
-    LC_COLLATE = "cs_CZ.UTF-8/UTF-8";
+    LC_ADDRESS = "cs_CZ.UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8";
+    LC_MEASUREMENT = "cs_CZ.UTF-8";
+    LC_MONETARY = "cs_CZ.UTF-8";
+    LC_NAME = "cs_CZ.UTF-8";
+    LC_NUMERIC = "cs_CZ.UTF-8";
+    LC_PAPER = "cs_CZ.UTF-8";
+    LC_TELEPHONE = "cs_CZ.UTF-8";
+    LC_TIME = "cs_CZ.UTF-8";
+    LC_MESSAGES = "cs_CZ.UTF-8";
+    LC_COLLATE = "cs_CZ.UTF-8";
   };
 
   # Bootloader
