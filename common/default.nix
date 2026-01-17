3f719b2cedfc09502a38f8f3fdcdfa809acbfe76
Dusan Tichy <tichy.dusan@proton.me>
2026-01-17 11:04:38 +0100

    fix czech locale wip

--- common/default.nix
+++ common/default.nix
@@ -4,19 +4,21 @@
   # Set your time zone.
   time.timeZone = "Europe/Prague";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocales = [ "cs_CZ.UTF-8/UTF-8" ];
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "cs_CZ.UTF-8";
-    LC_IDENTIFICATION = "cs_CZ.UTF-8";
-    LC_MEASUREMENT = "cs_CZ.UTF-8";
-    LC_MONETARY = "cs_CZ.UTF-8";
-    LC_NAME = "cs_CZ.UTF-8";
-    LC_NUMERIC = "cs_CZ.UTF-8";
-    LC_PAPER = "cs_CZ.UTF-8";
-    LC_TELEPHONE = "cs_CZ.UTF-8";
-    LC_TIME = "cs_CZ.UTF-8";
+    LC_ADDRESS = "cs_CZ.UTF-8/UTF-8";
+    LC_IDENTIFICATION = "cs_CZ.UTF-8/UTF-8";
+    LC_MEASUREMENT = "cs_CZ.UTF-8/UTF-8";
+    LC_MONETARY = "cs_CZ.UTF-8/UTF-8";
+    LC_NAME = "cs_CZ.UTF-8/UTF-8";
+    LC_NUMERIC = "cs_CZ.UTF-8/UTF-8";
+    LC_PAPER = "cs_CZ.UTF-8/UTF-8";
+    LC_TELEPHONE = "cs_CZ.UTF-8/UTF-8";
+    LC_TIME = "cs_CZ.UTF-8/UTF-8";
+    LC_MESSAGES = "cs_CZ.UTF-8/UTF-8";
+    LC_COLLATE = "cs_CZ.UTF-8/UTF-8";
   };
 
   # Bootloader
